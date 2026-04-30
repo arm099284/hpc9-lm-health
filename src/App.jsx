da300c77aeadb7d1637ffd114f7f9f83b90ca193
arm099284 <arm099284@gmail.com>
2026-04-30 09:30:53 +0700

    พื้มขนาดรูป ohs

--- src/App.jsx
+++ src/App.jsx
@@ -1778,11 +1778,11 @@ function OhsTable({ record }) {
         </span>
       </div>
 
-      <div className="relative aspect-square overflow-hidden rounded-xl bg-white ring-1 ring-slate-100">
+      <div className="relative aspect-square overflow-hidden rounded-xl bg-white ring-1 ring-slate-200 shadow-inner">
         <img
           src={src}
           alt={title}
-          className="h-full w-full object-contain p-1.5"
+          className="h-full w-full object-contain p-0"
           draggable={false}
         />
 
